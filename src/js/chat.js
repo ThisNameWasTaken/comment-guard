086f66ae3e7ff7ad71abd3119a320c8f612cafcc
Adrian <adriandinca2008@yahoo.com>
2019-08-16 18:02:34 +0300

    feat: Add chat when visiting profiles on large desktops

--- src/js/chat.js
+++ src/js/chat.js
@@ -1,11 +1,13 @@
 import userAgentOverride from './userAgent';
 import { rootStyles, chatStyles } from './styles';
 
+let chatIframe;
+
 const loadChat = () => {
   // Ignore load event from the chat iframe
   if (window.location.pathname.startsWith('/direct/inbox')) return;
 
-  const chatIframe = document.createElement('iframe');
+  chatIframe = document.createElement('iframe');
   chatIframe.src = 'https://instagram.com/direct/inbox';
   chatIframe.id = 'inbox-iframe';
   chatIframe.addEventListener('load', () => {
@@ -43,9 +45,13 @@ const loadChat = () => {
 
   if (reactRoot) {
     new MutationObserver(() => {
+      console.log('chat update');
+
       chatIframe.style.display = window.location.pathname.match(/^\/stories\/./)
         ? 'none'
         : '';
+
+      updateChat();
     }).observe(reactRoot, {
       childList: true,
       subtree: false,
@@ -58,101 +64,108 @@ window.addEventListener('load', loadChat, { once: true });
 
 let prevScrollY = window.scrollY;
 
-window.addEventListener('load', function() {
-  let instagramMainContent;
-  let isSmallDesktopScreen;
-  let leftSideGroupRect;
+let instagramMainContent;
+let isSmallDesktopScreen;
+let leftSideGroupRect;
 
-  function updateChatPosition() {
-    // Ignore load event from the chat iframe
-    if (window.location.pathname.startsWith('/direct/inbox')) return;
+function updateChatPosition() {
+  // Ignore load event from the chat iframe
+  if (window.location.pathname.startsWith('/direct/inbox')) return;
 
-    if (isSmallDesktopScreen) {
-      document.documentElement.style.setProperty(
-        '--chat-left',
-        `${leftSideGroupRect.left}px`
-      );
-
-      document.documentElement.style.setProperty(
-        '--chat-top',
-        `${leftSideGroupRect.top}px`
-      );
-
-      document.documentElement.style.setProperty(
-        '--chat-top-transition-duration',
-        '0s'
-      );
-
-      document.documentElement.style.setProperty(
-        '--chat-top-transition-delay',
-        '0s'
-      );
-    } else {
-      document.documentElement.style.setProperty('--chat-left', `0px`);
-
-      document.documentElement.style.setProperty(
-        '--chat-top',
-        `${Math.max(135 - window.scrollY, 52)}px`
-      );
-
-      const didScrollDown = window.scrollY > prevScrollY;
-
-      document.documentElement.style.setProperty(
-        '--chat-top-transition-duration',
-        didScrollDown ? '.2s' : '0s'
-      );
-
-      document.documentElement.style.setProperty(
-        '--chat-top-transition-delay',
-        didScrollDown ? '.2s' : '0s'
-      );
-    }
-
-    prevScrollY = window.scrollY;
-  }
-
-  const updateChatSize = () => {
-    // Ignore load event from the chat iframe
-    if (window.location.pathname.startsWith('/direct/inbox')) return;
-
-    if (!instagramMainContent) {
-      instagramMainContent = document.querySelector('main > * > *');
-    }
-
-    const leftSideGroup = document.querySelector(
-      'main > section > :last-of-type > :last-of-type'
+  if (isSmallDesktopScreen) {
+    document.documentElement.style.setProperty(
+      '--chat-left',
+      `${leftSideGroupRect.left}px`
     );
 
-    if (!leftSideGroup) return;
+    document.documentElement.style.setProperty(
+      '--chat-top',
+      `${leftSideGroupRect.top}px`
+    );
 
+    document.documentElement.style.setProperty(
+      '--chat-top-transition-duration',
+      '0s'
+    );
+
+    document.documentElement.style.setProperty(
+      '--chat-top-transition-delay',
+      '0s'
+    );
+  } else {
+    document.documentElement.style.setProperty('--chat-left', `0px`);
+
+    document.documentElement.style.setProperty(
+      '--chat-top',
+      `${Math.max(135 - window.scrollY, 52)}px`
+    );
+
+    const didScrollDown = window.scrollY > prevScrollY;
+
+    document.documentElement.style.setProperty(
+      '--chat-top-transition-duration',
+      didScrollDown ? '.2s' : '0s'
+    );
+
+    document.documentElement.style.setProperty(
+      '--chat-top-transition-delay',
+      didScrollDown ? '.2s' : '0s'
+    );
+  }
+
+  prevScrollY = window.scrollY;
+}
+
+function updateChatSize() {
+  // Ignore load event from the chat iframe
+  if (window.location.pathname.startsWith('/direct/inbox')) return;
+
+  instagramMainContent = document.querySelector('main > * > *');
+
+  const leftSideGroup = document.querySelector(
+    'main > section > :last-of-type > :last-of-type'
+  );
+
+  const _chatWidth = instagramMainContent.getBoundingClientRect().left - 28;
+
+  isSmallDesktopScreen = window.innerWidth < 1595;
+
+  if (leftSideGroup) {
     leftSideGroup.style.opacity = isSmallDesktopScreen ? '0' : '1';
 
     leftSideGroupRect = leftSideGroup.getBoundingClientRect();
 
-    const _chatWidth = instagramMainContent.getBoundingClientRect().left - 28;
+    chatIframe.style.display = '';
+  } else {
+    chatIframe.style.display = isSmallDesktopScreen ? 'none' : '';
+  }
 
-    isSmallDesktopScreen = _chatWidth <= leftSideGroupRect.width;
-
-    const chatWidth = isSmallDesktopScreen
+  const chatWidth = isSmallDesktopScreen
+    ? leftSideGroup
       ? leftSideGroupRect.width
-      : _chatWidth;
+      : 287
+    : _chatWidth;
 
-    document.documentElement.style.setProperty(
-      '--chat-width',
-      instagramMainContent ? `${chatWidth}px` : '20vw'
-    );
+  document.documentElement.style.setProperty(
+    '--chat-width',
+    instagramMainContent ? `${chatWidth}px` : '20vw'
+  );
 
-    document.documentElement.style.setProperty(
-      '--chat-height',
-      'calc(100vh - var(--chat-top))'
-    );
-  };
+  document.documentElement.style.setProperty(
+    '--chat-height',
+    'calc(100vh - var(--chat-top))'
+  );
+}
 
+function updateChat() {
   updateChatSize();
-  window.addEventListener('resize', updateChatSize, { passive: true });
-  window.addEventListener('resize', updateChatPosition, { passive: true });
-
   updateChatPosition();
-  window.addEventListener('scroll', updateChatSize, { passive: true });
-  window.addEventListener('scroll', updateChatPosition, { passive: true });
+}
+
+window.addEventListener('load', function() {
+  updateChat();
+
+  window.addEventListener('resize', updateChat, { passive: true });
+
+  window.addEventListener('scroll', updateChat, { passive: true });
 });
