68d41e399fd8c7113a20fc43a27105279f619f08
Adrian <adriandinca2008@yahoo.com>
2019-11-09 17:48:21 +0200

    feat: Add babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,33 @@
 import sass from 'rollup-plugin-sass';
 import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
 
 export default [
-  // Background
-  {
-    input: 'src/js/background.js',
-    output: {
-      file: 'build/background.js',
-      format: 'iife',
-    },
-    plugins: [resolve()],
-  },
-  // Content Script
-  {
-    input: 'src/js/content.js',
-    output: {
-      file: 'build/content.js',
-      format: 'iife',
-    },
-    plugins: [resolve()],
-  },
-  // Popup
-  {
-    input: 'src/js/popup.js',
-    output: {
-      file: 'build/popup.js',
-      format: 'iife',
-    },
-    plugins: [resolve(), sass({ output: true })],
-  },
+	// Background
+	{
+		input: 'src/js/background.js',
+		output: {
+			file: 'build/background.js',
+			format: 'iife',
+		},
+		plugins: [babel(), resolve()],
+	},
+	// Content Script
+	{
+		input: 'src/js/content.js',
+		output: {
+			file: 'build/content.js',
+			format: 'iife',
+		},
+		plugins: [babel(), resolve()],
+	},
+	// Popup
+	{
+		input: 'src/js/popup.js',
+		output: {
+			file: 'build/popup.js',
+			format: 'iife',
+		},
+		plugins: [babel(), resolve(), sass({ output: true })],
+	},
 ];
